c0c2912ef589632483760b8be3710d0c35121dcf
Venkata Mohan Sai <mohansaichintakrindi@gmail.com>
2026-02-19 22:35:05 +0530

    fix render dep port

--- app.py
+++ app.py
@@ -155,5 +155,6 @@ def uploaded_file(filename):
 
 # ---------- RUN ----------
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=True)
+    import os
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
